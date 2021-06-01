6db32875349e2505f07b33f0dd82477f03a54635
JuanMtzQ <trek202@outlook.com>
2021-06-01 18:52:51 -0500

    Clase 2. Ejercicio_01. Convertidor de monedas

---
+++ Clase_02/Ejercicio_01.rb
@@ -0,0 +1,5 @@
+puts "Conversor de monedas (S/ a USD)"
+puts "Ingrese la cantidad de soles: "
+soles = gets.to_f
+dolares = soles * 0.26
+puts "#{soles} soles equivalen a #{dolares} dólares americanos"
