32da24bcf1fb87bb4ba0f31329a1a5fad682543d
JuanMtzQ <trek202@outlook.com>
2021-06-01 17:09:59 -0500

    Ejercicio_01 (Rectangulo)

---
+++ Clase_01/Ejercicio_01.rb
@@ -0,0 +1,7 @@
+base = 10 
+altura = 50
+area = base * altura
+#Primera forma de expresar una combinación entre un String y un Int
+puts "El area del rectangulo es: " + area.to_s
+#Segunda forma de expresar una combinación entre un String y un Int
+puts "El area del rectangulo es: #{area}"
