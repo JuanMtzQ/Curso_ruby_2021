9c350e8b1272323476a5463d227c1f311930afc9
JuanMtzQ <trek202@outlook.com>
2021-06-03 16:40:46 -0500

    Agregando Ejercicio_05

---
+++ Clase_02/Ejercicio_05.rb
@@ -0,0 +1,12 @@
+#Imprimir 3 veces un mensaje
+#Primero, definimos un método
+#Método(Variables)
+def imprimir_mensaje(mensaje)
+  puts mensaje
+end
+#La variable contiene nuestro valor de interés
+mensaje = "Prueba de impresión multiple"
+#Se llama al método tres veces
+imprimir_mensaje(mensaje)
+imprimir_mensaje(mensaje)
+imprimir_mensaje(mensaje)
