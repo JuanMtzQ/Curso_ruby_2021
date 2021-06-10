641d369738d4e21ec2d1cd7b88ec3bfd2bf34c47
JuanMtzQ <trek202@outlook.com>
2021-06-10 17:45:47 -0500

    Codigo final

---
+++ Clase_04/Tareas/T4_E04.rb
@@ -0,0 +1,9 @@
+#Array de 10 valores númericos e impresión de su media
+puts "Los valores del array son: "
+valores = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+puts valores 
+valores.sum
+media = valores.sum / 10
+puts "La media es: "
+puts media 
+
