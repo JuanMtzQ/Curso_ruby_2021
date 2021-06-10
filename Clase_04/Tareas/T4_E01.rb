bc7e44827a4f40de982b29908ef880738164e616
JuanMtzQ <trek202@outlook.com>
2021-06-10 01:48:01 -0500

    Agregando 2do ejercicio de diapositiva 2

---
+++ Clase_04/Tareas/T4_E01.rb
@@ -0,0 +1,12 @@
+#Imprimir y contar los números múltiplos de 3 hasta un númeor ingresado
+puts "Ingrese un número por favor: "
+n = gets.chomp.to_i
+i = 0
+(1..n).each do |number|
+  if number % 3 == 0
+    puts number
+    i += 1
+  end
+end
+
+puts "Total de números multiplos de 3 es: #{i}"
