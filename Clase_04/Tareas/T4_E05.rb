d6b808d2f610ddb1587302a09aa088f5baeaa5e2
JuanMtzQ <trek202@outlook.com>
2021-06-10 17:55:56 -0500

    Codigo final

---
+++ Clase_04/Tareas/T4_E05.rb
@@ -0,0 +1,17 @@
+#Array de n valores númericos
+#Dado la suma de dichos valores, imprimir par o impar 
+puts "Ingrese 5 valores: "
+array = []
+v1 = gets.chomp.to_i
+v2 = gets.chomp.to_i
+v3 = gets.chomp.to_i
+v4 = gets.chomp.to_i
+v5 = gets.chomp.to_i
+"\n"
+array.push(v1,v2,v3,v4,v5)
+suma = array.sum
+if suma.even?
+  puts "Par"
+elsif suma.odd?
+  puts "Impar"
+end
