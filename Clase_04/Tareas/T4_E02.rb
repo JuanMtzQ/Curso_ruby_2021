b700622ebef2d093cf68f3dbf078a560608a1880
JuanMtzQ <trek202@outlook.com>
2021-06-15 16:43:26 -0500

    Codigo completo

--- Clase_04/Tareas/T4_E02.rb
+++ Clase_04/Tareas/T4_E02.rb
@@ -1,20 +1,27 @@
+#INCOMPLETO
 #Imprimir los números del 1 al 100. 
 #Además, calcular e imprimir la suma de todos los número pares e impares
-#INCOMPLETO
-i = 0
-(1..100).each do |number|
-puts number
+(1..100).each do |n|
+puts n
 end
 
 puts "\n"
 
-(1..100).each do |number|
-  if number.even?
-    sumpar = number.sum
-    puts sumpar
+array_par = []
+array_impar = []
+(1..100).each do |n|
+  if n.even?
+    array_par.push(n)
+  elsif 
+    array_impar.push(n)  
   end
 end
+puts "La suma de todos lo números pares de la sucesión anterior es: #{array_par.sum}"
+puts "La suma de todos lo números impares de la sucesión anterior es: #{array_impar.sum}"
 
+#Este paso no es necesario pero decidi incluirlo
+suma_total = array_par.sum + array_impar.sum
+puts "La suma de toda la sucesión es: #{suma_total}"
 
 
 
